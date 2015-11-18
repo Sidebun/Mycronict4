2970ae7a8c9bada5b6aaa582a0ad356ce854315c
Gustaf Edén <gustafeden@gmail.com>
2015-11-18 14:18:49 +0100

    initial git commit of rep

---
+++ cronic_changes/mall_cronic/js/change.js
@@ -0,0 +1,95 @@
+jQuery(function ($) {
+    // 'use strict';
+
+    $('#if_two').css({
+        opacity: 0
+    });
+
+
+
+
+    var data_treat, tx = 'list',
+        treated, numbers, numbers_treat, texts, texts_treat, url, time, i, pages = [];
+    //dong.get('main.php?first=' + tx);
+
+
+    jQuery.ajaxSetup({
+        async: false
+    });
+
+    $.get('main.php?first', function (data) {
+        data_treat = data;
+
+        console.log(typeof (data));
+        console.log("Load was performed.");
+        // console.log(dong + "dong");
+    });
+    jQuery.ajaxSetup({
+        async: true
+    });
+    // console.log(data_treat);
+
+
+    //    console.log(test + "test");
+    treated = data_treat.split('~');
+    texts_treat = treated[0];
+    numbers_treat = treated[1];
+    texts = texts_treat.split(' ');
+    numbers = numbers_treat.split(' ');
+
+
+    //    console.log("data treated");
+    //    console.log(texts.length + "\n" + numbers.length);
+
+
+    for (i = 0; i <= texts.length - 2; i++) {
+        //  finish[i] = new Array()
+        //    finish[i].page = treated[i];
+        pages[i] = {
+            page: texts[i],
+            dur: numbers[i]
+        };
+
+        // console.log("some");
+    }
+
+    //    console.log(JSON.stringify(pages));
+    //    console.log(pages);
+
+
+    var p = pages.length,
+        cif = 0;
+
+
+    while (--p > -1) {
+        (function (p) {
+            var a = document.createElement('a');
+            a.href = pages[p].page;
+            pages[p].page = a;
+        })(p);
+    }
+    $('iframe').load(function () {
+        $('iframe').not(this).animate({
+            opacity: 0
+        });
+        $(this).animate({
+            opacity: 1
+        }, 'slow', function () {
+            setTimeout(loadIframe, pages[p].dur);
+        });
+    });
+
+    function loadIframe() {
+
+        var page = pages[(p = ++p % pages.length)].page,
+            bust = 'bustcache=' + new Date().getTime();
+        page = page.search ? page.href + '&' + bust : page.href + '?' + bust;
+        $('iframe').eq(cif).attr('src', page);
+        cif = (++cif) % 2;
+    }
+    loadIframe();
+
+
+
+
+});
