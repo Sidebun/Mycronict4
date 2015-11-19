a4c4563735f821cb52b2aa34b59a98c2a4d87cca
Gustaf Edén <gustafeden@gmail.com>
2015-11-19 12:45:18 +0100

    fixes and added menu

--- mall_cronic/js/change.js
+++ mall_cronic/js/change.js
@@ -9,65 +9,76 @@ jQuery(function ($) {
 
     var data_treat, tx = 'list',
         pages = [];
-    //dong.get('main.php?first=' + tx);
+    var button = $('.js-button');
+    var dropdown) $('.dropdown');
+//dong.get('main.php?first=' + tx);
 
 
-    jQuery.ajaxSetup({
-        async: false
+jQuery.ajaxSetup({
+    async: false
+});
+
+$.get('main.php?sec', function (data) {
+    data_treat = jQuery.parseJSON(data);
+
+    // console.log(typeof (data));
+    console.log("Load was performed.");
+    // console.log(dong + "dong");
+
+
+});
+
+jQuery.ajaxSetup({
+    async: true
+}); console.log(data_treat); console.log(data_treat.length);
+
+pages = data_treat;
+
+console.log(pages);
+
+
+dropdown.click(function () {
+    dropdown.hide();
+    console.log('clicked');
+
+});
+
+
+
+
+
+
+var p = pages.length,
+    cif = 0;
+
+
+while (--p > -1) {
+    (function (p) {
+        var a = document.createElement('a');
+        a.href = pages[p].page;
+        pages[p].page = a;
+    })(p);
+}
+$('iframe').load(function () {
+    $('iframe').not(this).animate({
+        opacity: 0
     });
-
-    $.get('main.php?sec', function (data) {
-        data_treat = jQuery.parseJSON(data);
-
-        // console.log(typeof (data));
-        console.log("Load was performed.");
-        // console.log(dong + "dong");
-
-
+    $(this).animate({
+        opacity: 1
+    }, 'slow', function () {
+        setTimeout(loadIframe, pages[p].dur);
     });
+});
 
-    jQuery.ajaxSetup({
-        async: true
-    });
-    console.log(data_treat);
-    console.log(data_treat.length);
+function loadIframe() {
 
-    pages = data_treat;
-
-    console.log(pages);
-
-
-    var p = pages.length,
-        cif = 0;
-
-
-    while (--p > -1) {
-        (function (p) {
-            var a = document.createElement('a');
-            a.href = pages[p].page;
-            pages[p].page = a;
-        })(p);
-    }
-    $('iframe').load(function () {
-        $('iframe').not(this).animate({
-            opacity: 0
-        });
-        $(this).animate({
-            opacity: 1
-        }, 'slow', function () {
-            setTimeout(loadIframe, pages[p].dur);
-        });
-    });
-
-    function loadIframe() {
-
-        var page = pages[(p = ++p % pages.length)].page,
-            bust = 'bustcache=' + new Date().getTime();
-        page = page.search ? page.href + '&' + bust : page.href + '?' + bust;
-        $('iframe').eq(cif).attr('src', page);
-        cif = (++cif) % 2;
-    }
-    loadIframe();
+    var page = pages[(p = ++p % pages.length)].page,
+        bust = 'bustcache=' + new Date().getTime();
+    page = page.search ? page.href + '&' + bust : page.href + '?' + bust;
+    $('iframe').eq(cif).attr('src', page);
+    cif = (++cif) % 2;
+}
+loadIframe();
 
 
 
